443e0e8da5d25979d80caddf8b4260a08b21b735
Severi Hofslagare <zevez90@gmail.com>
2020-11-20 17:15:41 +0000

    2.5 tehty

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Header = ({course}) =>{
+    return(
+        <h1>{course.name}</h1>
+    )
+  }
+  
+  const Part = ( {parts} ) => {
+    return (
+      <div><ul><li>{parts.name}. {" "} {parts.exercises} exercises</li></ul></div>
+    )
+  }
+  const Content = ({course}) => {
+    return (
+      <div>{course.parts.map(parts =>
+      <Part key={parts.id} parts={parts}/> ) }
+        </div>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    const summa = course.parts.reduce((summa, part) => 
+     summa + part.exercises, 0)
+    return(
+      <p><b>Course has {summa} exercises.</b></p>
+    ) 
+  }
+  
+  const Course = ({course}) =>{
+    console.log(course)
+    return(
+        <div>
+          <Header course={course}/>
+          <Content course={course}/>
+          <Total course={course}/>
+        </div>
+    )
+  }
+
+  
+export default Course
--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,46 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = ({course}) =>{
-  return(
-      <h1>{course.name}</h1>
-  )
-}
-
-const Part = ({course}) => {
-  return (
-    <div>{course.parts.map(part =>
-    <Part key={parts.id} part={part}/>)}
-      </div>
-  )
-}
-const Content = ({course}) => {
-  return (
-    <div>
-      <p>{course.part}</p>
-    </div>
-  )
-}
-
-const Total = (course) => {
-  return (
-    <p>
-      Total number of excercises:  
-    </p>
-  )
-}
-
-const Course = ({course}) =>{
-  console.log(course)
-  return(
-      <div>
-        <Header course={course}/>
-        <Part course={course}/>
-      </div>
-  )
-}
-
-
+import Course from './components/Course'
 
 const App = () => {
 
@@ -64,6 +24,27 @@ const App = () => {
           exercises: 14,
           id: 3
         },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
       ]
     }
   ]
